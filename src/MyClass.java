c70a302131a86b05c304ea93b1c2af7f2c414b55
eserseyda <seydaarch@gmail.com>
2025-06-23 20:04:38 +0200

    v3

--- src/MyClass.java
+++ src/MyClass.java
@@ -6,4 +6,7 @@ public class MyClass {
     //Version v2
     public String string2 = "Bu da başka bir string'dir.";
 
+    //Version v3
+    public String string3 = "Bu da cok daha başka bir string'dir.";
+
 }
