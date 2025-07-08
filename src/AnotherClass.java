70d5b2817b147ec10d0256fe9efafb3cbb2bb20a
eserseyda <seydaarch@gmail.com>
2025-07-08 10:56:44 +0200

    t8

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -1,4 +1,7 @@
 public class AnotherClass {
     //Techpro branch,version t7
     public String string1 = "Biri sen, biri ben, iki damla yas aktı gözlerimden";
+
+    //Techpro branch, t8
+    public String string2 = "Parıldayıp duran insanlara bak";
 }
